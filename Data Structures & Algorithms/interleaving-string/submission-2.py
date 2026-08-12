69c5cc8bd9b6e53a32a00d93b1fbd863e565bd1c
Larry Tao <larrytao100@gmail.com>
2026-08-12 15:38:00 -0700

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        @lru_cache(maxsize=None)
+        def recurse(i,j):
+            if i == len(s1) and j == len(s2):
+               return True
+            k = i+j
+            if i < len(s1) and s1[i] == s3[k]:
+                if recurse(i+1,j):
+                    return True
+            if j < len(s2) and s2[j] == s3[k]:
+                if recurse(i,j+1):
+                    return True
+            return False
+        return recurse(0,0)
